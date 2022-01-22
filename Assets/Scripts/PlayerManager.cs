9719b8d413dbbf4b46c72e5d36991b3998ecbe35
Murilo Bezerra <murilozerra@gmail.com>
2022-01-22 14:03:10 -0300

    life ui

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -3,11 +3,19 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour
 {
     [SerializeField] private float life, maxLife, score = 0, points = 0, pointsToNextUpgrade;
+    public static PlayerManager instance;
 
-
+    LifeUi lifeUi;
+    private void Awake()
+    {
+        instance = this;
+    }
     private void Start()
     {
+        lifeUi = LifeUi.lifeUi;
         life = maxLife;
+
+        lifeUi.SetLifeBar(life,maxLife);
     }
     internal void TakeHit(float damage)
     {
@@ -16,10 +24,12 @@ public class PlayerManager : MonoBehaviour
         if(life <= 0)
         {
             life = 0;
-            
+            lifeUi.SetLifeBar(life, maxLife);
+
             GameOver();
             return;
         }
+        lifeUi.SetLifeBar(life, maxLife);
 
         points += damage; 
 
