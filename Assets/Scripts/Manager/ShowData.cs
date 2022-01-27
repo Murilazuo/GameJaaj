47ad22e69efccbcf6b78268c6424df779e6fe70f
iBeBoi <89034697+iBeBoi@users.noreply.github.com>
2022-01-27 09:58:51 -0300

    Ui game over

--- Assets/Scripts/Manager/ShowData.cs
+++ Assets/Scripts/Manager/ShowData.cs
@@ -5,21 +5,31 @@ public class ShowData : MonoBehaviour
 {
     [SerializeField] private string textBeforeScore, textBeforeLifeOrb, textBeforeTimeOrb, textBeforeLevel;
     [SerializeField] bool showScore;
+    [SerializeField] bool showHealthOrbs;
+    [SerializeField] bool showTimeOrbs;
 
     private void OnEnable()
     {
         if (showScore)
         {
-            GetComponent<Text>().text = textBeforeScore + 
+            GetComponent<Text>().text = textBeforeScore +
             Mathf.Round(PlayerManager.instance.GetComponent<ScoreCounter>().score);
+        }
+        else if (showHealthOrbs)
+        {
+            GetComponent<Text>().text =
+            textBeforeLifeOrb + "\n" +
+            PlayerManager.instance.GetComponent<PlayerCollision>().lifeOrb.ToString(); 
+        }
+        else if (showTimeOrbs)
+        {
+            GetComponent<Text>().text =
+            textBeforeTimeOrb + "\n" +
+            PlayerManager.instance.GetComponent<PlayerCollision>().timeOrb.ToString();
         }
         else
         {
             GetComponent<Text>().text =
-            textBeforeLifeOrb + "\n" +
-            PlayerManager.instance.GetComponent<PlayerCollision>().lifeOrb.ToString() + "\n\n" +
-            textBeforeTimeOrb + "\n" +
-            PlayerManager.instance.GetComponent<PlayerCollision>().timeOrb.ToString() + "\n\n" +
             textBeforeLevel + "\n" +
             FindObjectOfType<DisplayLevel>().GetLevel();
         }
