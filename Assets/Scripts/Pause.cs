8eeb14b3ee4ab076ecdc2643242cb5b173a47dae
Murilo Bezerra <murilozerra@gmail.com>
2022-01-29 09:01:28 -0300

    pause

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    bool pause;
+    [SerializeField]GameObject pausePanel;
+    private void Start()
+    {
+        UnPause();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pause)
+            {
+                UnPause();
+            }
+            else{
+                PauseGame();
+            }
+        }
+    }
+
+    void PauseGame()
+    {
+        pause = true;
+        pausePanel.SetActive(true);
+
+        Time.timeScale = 0;
+    }
+    public void UnPause()
+    {
+        pause = false;
+        pausePanel.SetActive(false);
+
+        Time.timeScale = 1;
+    }
+
+}
